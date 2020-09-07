c3757a19ffe31b698bd415749c721cee2e20a0d1
Joe-Thompson <bmo3696@gmail.com>
2020-09-06 20:25:49 -0400

    finished searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
-
+    if target <= end:
+        middle_index = (start + end) // 2
+        if arr[middle_index] == target:
+            return middle_index
+        elif target < arr[middle_index]:
+            left_end = middle_index - 1
+            return binary_search(arr, target, start, left_end)
+        else:
+            right_start = middle_index + 1
+            return binary_search(arr, target, right_start, end)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +19,5 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+# def agnostic_binary_search(arr, target):
+# Your code here
